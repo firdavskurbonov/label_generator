a6095ab4acd5866b339bb44298323e901784e068
firdavskurbonov <firdavs_k@mail.ru>
2025-12-02 20:57:26 +0500

    CORs

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     # In production, specify your frontend URL
     allow_origins=[
-        "https://label-generator-je72.onrender.com, https://label-generator-front.onrender.com"],
+        "https://label-generator-front.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -222,6 +222,6 @@ async def cleanup_old_files():
         raise HTTPException(status_code=500, detail=str(e))
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
